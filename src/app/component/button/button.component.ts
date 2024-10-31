c1503b45f465dff4e71dc13a37f168fa44aa0eea
Syeda Nabiha <159924237+Syeda-Nabihaa@users.noreply.github.com>
2024-10-31 18:55:49 +0500

    12th commit

--- src/app/component/button/button.component.ts
+++ src/app/component/button/button.component.ts
@@ -6,13 +6,14 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./button.component.css']
 })
 export class ButtonComponent {
-@Input({required:true}) buttonText:string=''; 
-@Input () buttonWidth:string='';
-@Input () buttonPx:string='';
-@Input () buttonPy:string='';
-@Input () buttonMt:string='';
-@Input () buttonMx:string='';
-@Input () buttonbgColor:string='';
-@Input () buttonColor:string='';
-
-}
+  @Input() buttonText: string = '';      
+  @Input() buttonWidth: string = '';     
+  @Input() buttonPx: string = '';        
+  @Input() buttonIcon: string = '';      
+  @Input() buttonPy: string = '';        
+  @Input() buttonMt: string = '';        
+  @Input() buttonMx: string = '';        
+  @Input() buttonbgColor: string = '';   
+  @Input() buttonColor: string = '';     
+  @Input() textSize: string = '';     
+}  
